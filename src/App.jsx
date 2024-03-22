7bf3ac7e42e0df437ba1dd411fbe88afdb56ebb9
Hellcat232 <maxkuich1993@gmail.com>
2024-03-22 18:58:53 +0200

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import "./App.css";
+import Profile from "./components/Profile";
+import userData from "./userData.json";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        stats={userData.stats}
+        location={userData.location}
+        avatar={userData.avatar}
+      />
+    </div>
+  );
+}
 
 export default App;

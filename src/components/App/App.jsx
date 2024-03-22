5c74e6ed1db9e1f63a27b63d366075ab5be41ecb
Hellcat232 <maxkuich1993@gmail.com>
2024-03-22 21:18:27 +0200

    asd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,10 @@
-import Profile from "./components/Profile";
-import userData from "./userData.json";
+import Profile from "../Profile/Profile.jsx";
+import userData from "../../userData.json";
+import css from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={css["main-app"]}>
       <Profile
         username={userData.username}
         tag={userData.tag}
